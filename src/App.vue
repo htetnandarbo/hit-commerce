ad181ec100428f081a6aca35ce7be00a00445533
htetnandarbo <htetnandarbo8@gmail.com>
2024-01-27 12:09:32 +0630

    hndb

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="grid grid-cols-3">
+      <div>
+        <h1>Shopy</h1>
+      </div>
+      <div>
+        <div>
+          <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
