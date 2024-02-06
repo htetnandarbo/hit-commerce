4258e567666862741d9833a8ec951a801fa12779
Jarvis <jarvis644171@gmail.com>
2024-02-06 11:55:27 +0630

    test

--- src/router/index.js
+++ src/router/index.js
@@ -4,22 +4,32 @@ import { createRouter, createWebHistory } from "vue-router";
 //step1
 const Home = () => import("./../pages/Home.vue");
 const Cart = () => import("./../pages/Cart.vue");
+const Products = () => import("./../pages/Products.vue");
+const ProductDetails = () => import("./../pages/Product-details.vue");
 
 //Step2
-const routes=[
-  
-    {
-        path:'/home',
-        name:'home',
-        component: Home
-    },
-    {
-        path:'/cart',
-        name:'cart',
-        component: Cart
-    },
-    
-]
+const routes = [
+  {
+    path: "/home",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: Cart,
+  },
+  {
+    path: "/products",
+    name: "products",
+    component: Products,
+  },
+  {
+    path: "/product-details",
+    name: "product-details",
+    component: ProductDetails,
+  },
+];
 
 //Step 3
 const router = createRouter({
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      lineHeight: {
+        "3rem": "3rem",
+      },
+    },
   },
   plugins: [require("daisyui")],
-}
-
+};
